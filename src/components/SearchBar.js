a192b5ae787eb7c343bd75b03ac2874d3b3b3b62
Zeki Unyildiz <zekiunyildiz@gmail.com>
2021-03-28 14:12:35 +0300

    Proje iskelet oluşturduk.

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Hello SearchBar</h1>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
